a9f056c5478bd4f678777b9b293b6a5fe4cb4065
JianpingWang <172099994@qq.com>
2020-06-28 16:42:05 +0800

    Add files via upload

---
+++ learn01.py
@@ -0,0 +1,3 @@
+age = 23   #小节字符处理  数转字符处理  
+print("happy " + str(age) + " birthday")
+
---
+++ leran00.py
@@ -0,0 +1,37 @@
+print("hello world")    #20200616 正式开始学习python  第一小节字符处理
+print("My name is WangJianping")
+
+message = "Hello python world"
+print (message)
+message = "Hello python crash course world"
+print (message)
+
+message = "DarkGold"
+print(message)
+
+message = "This is a string"
+print(message)
+message = 'This is also a string.'
+print(message.title())
+print(message.upper())
+print(message.lower())
+
+first_name = "Wang"
+last_name = "Jianping"
+full_name = first_name + " " + last_name
+print(full_name)
+print("hello, " + full_name.upper() + "!")
+message = "hello, " + full_name.lower() + "!"
+print(message)
+
+print("\tprint")
+
+print("Jian:\n\twang\n\tC\n\tping")
+
+favorite_language = 'python '
+print(favorite_language)
+print(favorite_language.rstrip())
+print(favorite_language)
+
+message = "My name's wang"
+print(message)
