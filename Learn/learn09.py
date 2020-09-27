1bb82541f23c989df881dae50c9c6935807a8cea
MasterWangdaoyong <31734439+MasterWangdaoyong@users.noreply.github.com>
2020-09-27 23:15:12 +0800

    20200927的学习

--- Learn/learn09.py
+++ Learn/learn09.py
@@ -46,4 +46,75 @@
 # your_dog.sit()
 
 
-# 9.2.1  Car 类
+# 9.2.1  Car 类     20200927
+# class Car():
+#     def __init__(self, make, model, year):
+#         """初始化描述汽车的属性"""
+#         self.make = make
+#         self.model = model
+#         self.year = year
+    
+#     def get_descriptive_name(self):
+#         """返回整洁的描述性信息"""
+#         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+#         return long_name.title()
+    
+# my_new_car = Car('audi', 'a4', 2016)
+# print(my_new_car.get_descriptive_name())
+
+
+# 9.2.2  给属性指定默认值
+# class Car():
+#     def __init__(self, make, model, year):
+#         """初始化描述汽车的属性"""
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0   #可以理解成私有变量
+    
+#     def get_descriptive_name(self):
+#         """返回整洁的描述性信息"""
+#         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+#         return long_name.title()
+    
+#     def read_odometer(self):        #调用了私用变量
+#         """打印一条指出汽车里程的消息"""
+#         print("This car has " + str(self.odometer_reading) + " miles on it.")
+    
+# my_new_car = Car('audi', 'a4', 2016)
+# print(my_new_car.get_descriptive_name())
+# my_new_car.read_odometer()          
+
+
+# 9.2.3  修改属性的值
+# class Car():
+#     def __init__(self, make, model, year):
+#         """初始化描述汽车的属性"""
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0   #可以理解成私有变量
+    
+#     def get_descriptive_name(self):
+#         """返回整洁的描述性信息"""
+#         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+#         return long_name.title()
+    
+#     def read_odometer(self):        #调用了私用变量
+#         """打印一条指出汽车里程的消息"""
+#         print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+#     def updata_odometer(self, mileage):
+#         self.odometer_reading = mileage    
+    
+# my_new_car = Car('audi', 'a4', 2016)
+# print(my_new_car.get_descriptive_name())
+# my_new_car.read_odometer()  
+# my_new_car.odometer_reading = 23
+# my_new_car.read_odometer() 
+# my_new_car.updata_odometer(33)
+# my_new_car.read_odometer() 
+
+
+# 9.3  继承
+# 9.3.1  子类的方法  __init__()
